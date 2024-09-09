6757076976be325d90c4cc9cfec0f71e11174ad5
pratikraj95 <rajpratik880@gmail.com>
2024-09-09 21:42:08 +0530

    alert commit

--- js/script.js
+++ js/script.js
@@ -1,39 +1,42 @@
-var swiper = new Swiper(".slide-content", {
-  slidesPerView: 3,
-  spaceBetween: 25,
-  loop: true,
-  centerSlide: 'true',
-  fade: 'true',
-  grabCursor: 'true',
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    dynamicBullets: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
 
-  breakpoints:{
-      0: {
-          slidesPerView: 1,
-      },
-      678: {
-          slidesPerView: 2,
-      },
-      1170: {
-          slidesPerView: 3,
-      },
+// Initialize Swiper
+var swiper = new Swiper(".slide-content", {
+slidesPerView: 3,
+spaceBetween: 25,
+loop: true,
+centerSlide: true,
+fade: true,
+grabCursor: true,
+pagination: {
+  el: ".swiper-pagination",
+  clickable: true,
+  dynamicBullets: true,
+},
+navigation: {
+  nextEl: ".swiper-button-next",
+  prevEl: ".swiper-button-prev",
+},
+breakpoints: {
+  0: {
+    slidesPerView: 1,
   },
+  678: {
+    slidesPerView: 2,
+  },
+  1170: {
+    slidesPerView: 3,
+  },
+},
 });
+
+// Event listeners for buttons
 document.querySelectorAll('.button').forEach(button => {
-  button.addEventListener('pointerdown', (event) => {
-      event.stopPropagation();
-  });
-  button.addEventListener('click', (event) => {
-      event.preventDefault();
-      var targetModal = event.currentTarget.getAttribute('data-bs-target');
-      $(targetModal).modal('show');
-  });
+button.addEventListener('pointerdown', (event) => {
+  event.stopPropagation();
+});
+button.addEventListener('click', (event) => {
+  event.preventDefault();
+  var targetModal = event.currentTarget.getAttribute('data-bs-target');
+  $(targetModal).modal('show');
+});
 });
